bc087ed04a28a601cb6cc400adbc5be97556b39c
Chris Dent <chris.dent@gmail.com>
2013-01-16 11:22:11 +0000

    release 1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ AUTHOR = 'Chris Dent'
 AUTHOR_EMAIL = 'cdent@peermore.com'
 NAME = 'tiddlywebplugins.links'
 DESCRIPTION = 'Maintain a tiddlers links database'
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 
 
 import os
